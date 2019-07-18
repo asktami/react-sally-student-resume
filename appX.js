fc018b4feeb352c55684d4d5008db567b2a091e1
Tami Williams <tami@asktami.com>
2019-07-17 21:16:06 -0400

    updated app.js

--- appX.js
+++ appX.js
@@ -1,54 +1,52 @@
 'use strict';
 
 // App Component
+// everything inside a DIV because multiple sibling sections
 function App(props) {
 	return (
-		<div className={'container'}>
-			<header>
+		<div className="container">
+		<header>
 				<nav>
 					<ul>
 						<li>
-							<a href={'#'} className={'active'}>About Me (React)</a>
+							<a href='#' className='active'>About Me (React)</a>
 						</li>
 						<li>
-							<a href={'Sally_Student_Resume_Semantic.html'}>Not React</a>
+							<a href='Sally_Student_Resume_Semantic.html'>Not React</a>
 						</li>
 					</ul>
 				</nav>
 
 				<h1>{props.name}</h1>
 				<address>
-					<a href={'mailto:sally@sally.com'} target={'_blank'}>{props.email}</a>
+					<a href={'mailto:' + this.props.email} target='_blank'>{props.email}</a>
 					<br />
-					<a href={'tel:1-111-222-3344'}>{props.phone}</a>
+					<a href={'tel:' + this.props.phone}>{props.phone}</a>
 					<br />
-					<a href={props.url} target={'_blank'}>{props.url}</a>
+					<a href={props.url} target='_blank'>{props.url}</a>
 					<br />
-					<a href={'https://github.com/sally'} target={'_blank'}>{props.github}</a>
+					<a href={props.github} target='_blank'>{props.github}</a>
 				</address>
 				<br />
-				<img
-					src={'https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg'}
-					alt={'Student Sally Picture'}
-				/>
+				<img src='https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg' alt='Student Sally Picture' />
 			</header>
 
 			<main>
-				<section id={'summary'}>
+				<section id='summary'>
 					<header><h2>Summary</h2></header>
 					<p>
 						Background summary...
 				</p>
 				</section>
 
-				<section id={'education'}>
+				<section id='education'>
 					<header><h2>Education</h2></header>
 					<ul>
 						<li>Cornell University, B.S 1992</li>
 					</ul>
 				</section>
 
-				<section id={'skills'}>
+				<section id='skills'>
 					<header>
 						<h2>Technical Skills</h2>
 					</header>
@@ -62,16 +60,16 @@ function App(props) {
 					</ul>
 				</section>
 
-				<section id={'experience'}>
+				<section id='experience'>
 					<header>
 						<h2>
 							Work Experience
-						<a href={'https://sally.com/work/'} target={'_blank'}>[more]</a>
+						<a href='https://sally.com/work/' target='_blank'>[more]</a>
 						</h2>
 					</header>
 
 					<details open>
-						<summary>XXX Company (<a href={'http://xxx.com/'} target={'_blank'}
+						<summary>XXX Company (<a href='http://xxx.com/' target='_blank'
 						>http://xxx.com</a
 						>), Junior Developer 2014 - 2015
 					</summary>
@@ -92,7 +90,7 @@ function App(props) {
 					</details>
 
 					<details open>
-						<summary>ZZZ Company (<a href={'http://zzz.com'} target={'_blank'}
+						<summary>ZZZ Company (<a href='http://zzz.com' target='_blank'
 						>http://zzz.com</a
 						>), Senior 2015 - 2017
 					</summary>
@@ -113,28 +111,28 @@ function App(props) {
 					</details>
 				</section>
 
-				<section id={'publications'}>
+				<section id='publications'>
 					<h2>
 						Selected Publications
-					<a href={'https://sally.com/publications'} target={'_blank'}>[more]</a>
+					<a href='https://sally.com/publications' target='_blank'>[more]</a>
 					</h2>
 					<ul>
 						<li>
-							<a href={'https://sally.com/publicationName'} target={'_blank'}
+							<a href='https://sally.com/publicationName' target='_blank'
 							>Publication Name</a
 							>, April 2019
 					</li>
 					</ul>
 				</section>
 
-				<section id={'articles'}>
+				<section id='articles'>
 					<h2>
 						Selected Articles
-					<a href={'https://sally.com/articles'} target={'_blank'}>[more]</a>
+					<a href='https://sally.com/articles' target='_blank'>[more]</a>
 					</h2>
 					<ul>
 						<li>
-							<a href={'https://sally.com/articleName'} target={'_blank'}
+							<a href='https://sally.com/articleName' target='_blank'
 							>Article Name</a
 							>, April 2019
 					</li>
@@ -145,7 +143,6 @@ function App(props) {
 			<footer>
 				&copy; 2019 {props.name}
 			</footer>
-
 		</div>
 	);
 };
